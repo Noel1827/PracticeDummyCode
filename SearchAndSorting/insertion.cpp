9f5abe37b4647d3170ee470aed29c065e5d069d1
Noel1827 <noel.vargas5@upr.edu>
2020-11-12 19:55:29 -0400

    Some Sorting Code

---
+++ SearchAndSorting/insertion.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <cmath>
+
+// #include <bits/stdc++.h> 
+// using namespace std;
+
+// int insertionSort(vector<int> v)
+// {
+//     vector<int> New;
+//     New.push_back(v.at(0));
+//     for (int i = 1; i < v.size(); i++)
+//     {
+//         if (v[0 ]> v[i] ){
+     
+//         }
+//     }
+// }
+
+// int main()  
+// {  
+//     vector<int> v = { 12, 11, 13, 5, 6 };  
+  
+//     insertionSort(v);  
+     
+  
+//     return 0;  
+// }  
